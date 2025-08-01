caa5305ae7bb7a5314bb5a5ed1d7c32c6e2a3396
Brian <briankuria7@gmail.com>
2025-08-02 00:44:31 +0300

    logic update

--- hobby_picker/data.js
+++ hobby_picker/data.js
@@ -4,6 +4,7 @@ export const data = [
     age: 28,
     location: "Nairobi",
     hobbies: ["yoga", "cooking", "gardening"],
+    picture: "./Assets/sophia.webp",
     languages: ["English", "Swahili"]
   },
   {
@@ -11,6 +12,7 @@ export const data = [
     age: 34,
     location: "Mombasa",
     hobbies: ["photography", "cycling"],
+    picture: "./Assets/david.jpeg",
     languages: ["English", "French"]
   },
   {
@@ -18,6 +20,7 @@ export const data = [
     age: 22,
     location: "Kisumu",
     hobbies: ["makeup", "vlogging", "traveling"],
+    picture: "./Assets/Aisha.avif",
     languages: ["Swahili", "Spanish"]
   },
   {
@@ -25,6 +28,7 @@ export const data = [
     age: 40,
     location: "Nakuru",
     hobbies: ["fishing", "coding", "chess"],
+    picture: "./Assets/james.jpg",
     languages: ["English", "German", "Swahili"]
   },
   {
@@ -32,7 +36,39 @@ export const data = [
     age: 30,
     location: "Eldoret",
     hobbies: ["running", "baking"],
+    picture: "./Assets/grace.webp",
     languages: ["English"]
+  },
+  {
+    name: "Linda",
+    age: 26,
+    location: "Naivasha",
+    hobbies: ["cooking", "cycling", "traveling"],
+    picture: "./Assets/linda.webp",
+    languages: ["English", "Swahili"]
+  },
+  {
+    name: "Brian",
+    age: 32,
+    location: "Machakos",
+    hobbies: ["chess", "cycling", "baking"],
+    picture: "./Assets/Brian.avif",
+    languages: ["English", "German"]
+  },
+  {
+    name: "Nancy",
+    age: 29,
+    location: "Thika",
+    hobbies: ["yoga", "vlogging", "gardening"],
+    picture: "./Assets/nancy.jpg",
+    languages: ["Swahili", "Spanish"]
+  },
+  {
+    name: "Peter",
+    age: 38,
+    location: "Kitale",
+    hobbies: ["fishing", "coding", "running"],
+    picture: "./Assets/Peter.jpeg",
+    languages: ["English", "Swahili"]
   }
 ];
-
--- hobby_picker/index.js
+++ hobby_picker/index.js
@@ -1,13 +1,26 @@
 import { data } from './data.js'
 
 const hobbiesContainer = document.getElementById('hobbiesContainer');
+const getPartnerBtn = document.getElementById('getPartner-btn')
+
+//function to remove duplicate hobbies. this function is an array
+function removeDoubles() {
+    let hobbiesArray = []
+    for (let person of data) {
+        for (let hobby of person.hobbies) {
+            if (!hobbiesArray.includes(hobby)) {
+                hobbiesArray.push(hobby)
+            }
+        }
+     }
+     return hobbiesArray //returns an array
+}
 
 // loop through the data and render out each hobby
 function render() {
     let allhobbies = ''
-    for (let person of data) {
-        for (let hobby of person.hobbies)
-            allhobbies += `
+    for (let hobby of removeDoubles()) { 
+        allhobbies += `
         <div class="hobby">
             <label for="${hobby}">${hobby}</label>
             <input
@@ -19,18 +32,77 @@ function render() {
         </div>`
     }
     hobbiesContainer.innerHTML = allhobbies
-}
+ }
 
 render()
 
 //select one single hobby, collect the id of selected and highlight selection.
-hobbiesContainer.addEventListener('click', (e)=> {
-    const hobbyId = document.getElementById(e.target.id);
-    // console.log(hobbyId.parentElement)
-    hobbyId.parentElement.classList.add('highlight');  
+function highlightchecked(e) {
+    const hobbys = document.getElementsByClassName('hobby')
+    for (let hobby of hobbys) {
+        hobby.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+//listening for change of radio buttons within the contaimer
+hobbiesContainer.addEventListener('change', highlightchecked)
+
+//check if the the radio has been checked when clicking the button and checkbox checked
+//button click
+getPartnerBtn.addEventListener('click', ()=> {
+    // function. check which radio is checked
+    function radioCheck() { //returns a value e.g coding
+        const isRadioChecked= document.querySelector('input[type="radio"]:checked');
+        if(isRadioChecked) {
+            return isRadioChecked.value //returns e.g coding, vlogging...
+        }
+    }
+
+    // check if checkbox is checked and return the filtered list
+    function boxCheck() { //returns an array of people who speak english. has objects inside 
+        const isBoxChecked = document.querySelector('input[type="checkbox"]:checked');
+        if (isBoxChecked) {
+            const language = data.filter( (set)=> {
+                return set.languages.includes("English")
+            })
+            return language
+        }
+    }
+
+    // function to return people with shared hobby
+    function shareHobby() { //returns an array of people who share checked hobby
+        const hobbyPeople = data.filter( (set)=> {
+            return set.hobbies.includes(radioCheck())
+        })
+        return hobbyPeople 
+    }
+
+    //function if people select both radio and checkbox
+    function newArrays() { //returns a combined array of english people and shared hobby people
+        let newArray = boxCheck()
+        for (let share of shareHobby()) {
+            newArray.push(share)
+        }
+        return newArray // array of both english and hobbie people but has duplicates
+    }
+
+    console.log(newArrays())
+
+    // const oldArrays = data.filter( (set)=> {
+    //     newArray.push( set.languages.includes("English") )
+    // })
+
+
+    //if radio and/or box are checked
+    // if ( radioCheck() && boxCheck() ) {
+    //     console.log('both checked')
+    // } else if (radioCheck()){
+    //     console.log('radio checked')
+    // } else {
+    //     console.log('you have to select a hobby')
+    // }
+
 });
 
-const individualHobby = document.getElementsByClassName('hobby');
-for (let eachHobby of individualHobby) {
-    eachHobby.parentElement.classList.remove('highlight')
-}
+
